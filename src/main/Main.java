4f3c0e16a9a2251c926971c4a6565e130493d1e9
Rwest77 <42978366+Rwest77@users.noreply.github.com>
2019-10-01 13:43:28 -0600

    Continued working on 'createSets' function

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,11 +7,29 @@ import java.util.Scanner;
 public class Main {
 	
 	public void createSets(String filename){
+		int sizeUniSet;
+		int numSubsets;
+		String currentLine;
 		FileReader reader;
 		Scanner scan;
+		
 		try{
 			reader = new FileReader(filename);
 			scan = new Scanner(reader);
+			
+			//get Universal set and number of subsets
+			sizeUniSet = scan.nextInt();
+			numSubsets = scan.nextInt();
+			
+			//keep going if there is another line with another set
+			while(scan.hasNextLine()){
+				currentLine = scan.nextLine();
+				System.out.println("currentLine is: " + currentLine);
+				
+				String[] temp= currentLine.split(" ");
+				int[] values = new int[temp.length];
+				
+			}
 		}
 		catch (FileNotFoundException e){
 			System.out.println("File not found");
@@ -19,7 +37,8 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-		
+		Main prog = new Main();
+		prog.createSets("Input file");
 	}
 
 }
