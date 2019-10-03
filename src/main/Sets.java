325b62563ed17958b2b50ee5771c51bdf52fde0f
Rwest77 <42978366+Rwest77@users.noreply.github.com>
2019-10-02 21:23:40 -0600

    continued working on 'createSets' function

--- src/main/Sets.java
+++ src/main/Sets.java
@@ -2,11 +2,24 @@ package main;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Sets {
 	int sizeUniSet;
 	int numSubsets;
+	
+	Set<Integer> universalSet;
+	ArrayList<int[]> subsetsList;
+	ArrayList<Integer> weightsList;
+
+	public Sets(){
+		subsetsList = new ArrayList<int[]>();
+		weightsList = new ArrayList<Integer>();
+		universalSet = new HashSet<Integer>();
+	}
 	
 	public void createSets(String filename){
 
@@ -24,7 +37,9 @@ public class Sets {
 			numSubsets = scan.nextInt();
 			scan.nextLine();
 			
-			System.out.println(sizeUniSet + ", " + numSubsets);
+			//create the universal set
+			
+			
 			//keep going if there is another line with another set
 			while(scan.hasNextLine()){
 				currentLine = scan.nextLine();
@@ -32,18 +47,23 @@ public class Sets {
 				
 				//parse the line with a space as the delimiter
 				String[] temp= currentLine.split(" ");
-				System.out.println(temp[0]);
 				int[] values = new int[temp.length];
 				
 				for(int i = 0; i < values.length; i++){
 					values[i] = Integer.parseInt(temp[i]);
 				}
-				scan.nextLine();
+				System.out.println(values[0]);
+				//scan.nextLine();
+				weights.add(scan.nextInt());
 			}
 		}
 		
 		catch (FileNotFoundException e){
 			System.out.println("File not found");
 		}
+	}
+	
+	public void checkSets(){
+		System.out.println("Universal set is: ");
 	}
 }
